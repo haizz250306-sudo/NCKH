0a5bdb4e4ea103f410e99ff15e54441823e8c984
Nam <haizz250306@gmail.com>
2026-01-31 01:51:27 +0700

    nội dung ghi chú

--- main/index.js
+++ main/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const morgan = require('morgan');
-const hbs = require('express-handlebars');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const hbs = require("express-handlebars");
+const path = require("path");
 const app = express();
 const port = 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   express.urlencoded({
     extended: true,
@@ -13,29 +13,29 @@ app.use(
 );
 app.use(express.json());
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 app.engine(
-  'hbs',
+  "hbs",
   hbs.engine({
-    extname: '.hbs',
+    extname: ".hbs",
   }),
 );
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-  app.get('/', (req, res) => {
-  res.render('home');
+    app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get('/search', (req, res) => {
+app.get("/search", (req, res) => {
   console.log(req.query);
-  res.render('search');
+  res.render("search");
 });
 
-app.post('/hi', (req, res) => {
-  res.render('https://www.youtube.com/?app=desktop&hl=vi');
+app.post("/hi", (req, res) => {
+  res.render("https://www.youtube.com/?app=desktop&hl=vi");
 });
 
 app.listen(port, () => {
